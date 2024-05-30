96ea313e6d2f63f6335424c8a81c1a6186f4ee64
HassanSheikh1033 <153464118+HassanSheikh1033@users.noreply.github.com>
2024-05-30 22:13:21 +0500

    Initial commit

---
+++ lib/actions/category.actions.ts
@@ -0,0 +1,47 @@
+"use server"
+
+import { CreateCategoryParams } from "@/types"
+import { handleError } from "../utils"
+import { connectToDatabase } from "../database"
+import Category from "../database/models/category.model"
+
+
+
+
+// CREATE CATEGORIES ========================= 
+export const createCategory = async ({ categoryName }: CreateCategoryParams) => {
+  try {
+    await connectToDatabase();
+
+    const newCategory = await Category.create({ name: categoryName });
+
+    return JSON.parse(JSON.stringify(newCategory));
+  } catch (error) {
+    handleError(error)
+  }
+}
+
+
+
+
+
+// GET ALL CATEGORIES ========================= 
+export const getAllCategories = async () => {
+  try {
+    await connectToDatabase();
+
+    const categories = await Category.find();
+
+    return JSON.parse(JSON.stringify(categories));
+  } catch (error) {
+    handleError(error)
+  }
+}
+
+
+
+
+
+
+
+
